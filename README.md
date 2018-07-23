236d1d23353fc5ec9cb5333f00bf0572f01ae87e
Dipta Das <dipta670@gmail.com>
2018-07-23 16:36:16 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# KubeCI Test
---
+++ math_test.go
@@ -0,0 +1,22 @@
+package test
+
+import "testing"
+
+type testPair struct {
+	input  []int
+	output int
+}
+
+var tests = []testPair{
+	{[]int{1, 2, 3}, 6}, // pass
+	{[]int{1, 2, 3}, 5}, // fail
+}
+
+func TestSum(t *testing.T) {
+	for _, item := range tests {
+		ret := Sum(item.input)
+		if ret != item.output {
+			t.Errorf("Expected %v, got %v", item.output, ret)
+		}
+	}
+}
