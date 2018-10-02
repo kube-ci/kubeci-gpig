0405cfe714fe1994efb90937a5a207a475210dc7
diptadas <dipta670@gmail.com>
2018-10-02 18:47:20 +0600

    Update test script

--- test.sh
+++ test.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 
+# install curl
+apk --update add curl
+
+# required environment variables
+
+TARGET_URL=https://kube.ci
+REPO=diptadas/kubeci-gpig
+CONTEXT=kubeci
+
+STATE_PENDING=pending
+STATE_SUCCESS=success
+STATE_FAILURE=failure
+DESCRIPTION_PENDING=kubeci-build-pending
+DESCRIPTION_SUCCESS=kubeci-build-success
+DESCRIPTION_FAILURE=kubeci-build-failure
+
+# HEAD_SHA
+# TOKEN
+# PR_NUMBER
+
 # githhub status api post data
 
-generate_post_data() {
+generate_status() {
 	cat <<EOF
 {
   "state": "$STATE",
@@ -17,24 +37,25 @@ API_URL=https://api.github.com/repos/${REPO}/statuses/${HEAD_SHA}?access_token=$
 
 # set pending status
 
-STATE='pending'
+STATE=$STATE_PENDING
 DESCRIPTION=$DESCRIPTION_PENDING
-curl -X POST -d "$(generate_post_data)" "${API_URL}"
+curl -X POST -d "$(generate_status)" "${API_URL}"
 
 # run tests
+
 go test -v
 
 # set succeed/failed status
 
 if [ $? -eq 0 ]; then
-	STATE='success'
+	STATE=$STATE_SUCCESS
 	DESCRIPTION=$DESCRIPTION_SUCCESS
 else
-	STATE='failure'
+	STATE=$STATE_FAILURE
 	DESCRIPTION=$DESCRIPTION_FAILURE
 fi
 
-curl -X POST -d "$(generate_post_data)" $API_URL
+curl -X POST -d "$(generate_status)" $API_URL
 
 # remove ok-to-test label
 
