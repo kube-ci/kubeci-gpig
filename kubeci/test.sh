ef96193e5bb9b3d95e859300670a19f0de38ed7f
diptadas <dipta670@gmail.com>
2018-10-03 13:14:16 +0600

    Add kubeci setup artifacts

--- kubeci/test.sh
+++ kubeci/test.sh
@@ -5,7 +5,7 @@ apk --update add curl
 
 # required environment variables
 
-TARGET_URL=https://kube.ci
+TARGET_URL=http://127.0.0.1:9090/namespaces/${NAMESPACE}/workplans/${WORKPLAN}
 REPO=diptadas/kubeci-gpig
 CONTEXT=kubeci
 
@@ -36,14 +36,26 @@ EOF
 API_URL=https://api.github.com/repos/${REPO}/statuses/${HEAD_SHA}?access_token=${TOKEN}
 
 # set pending status
+echo "setting pending status..."
 
 STATE=$STATE_PENDING
 DESCRIPTION=$DESCRIPTION_PENDING
-curl -X POST -d "$(generate_status)" "${API_URL}"
+curl -s -o /dev/null -X POST -d "$(generate_status)" $API_URL
+
+# link repository files to package path
+
+mkdir -p $GOPATH/src/github.com/diptadas/
+ln -s /kubeci/workspace $GOPATH/src/github.com/diptadas/kubeci-gpig
 
 # run tests
+echo "running go tests..."
 
-go test -v
+cd $GOPATH/src/github.com/diptadas/kubeci-gpig
+go test ./...
+
+# just wait to see log stream working or not
+echo "waiting for 30s..."
+sleep 30s
 
 # set succeed/failed status
 
@@ -55,9 +67,13 @@ else
 	DESCRIPTION=$DESCRIPTION_FAILURE
 fi
 
-curl -X POST -d "$(generate_status)" $API_URL
+echo "setting succeed/failed status..."
+curl -s -o /dev/null -X POST -d "$(generate_status)" $API_URL
 
 # remove ok-to-test label
+echo "removing ok-to-test label..."
 
 API_URL=https://api.github.com/repos/${REPO}/issues/${PR_NUMBER}/labels/ok-to-test?access_token=${TOKEN}
-curl -X DELETE $API_URL
+curl -s -o /dev/null -X DELETE $API_URL
+
+echo "done"
